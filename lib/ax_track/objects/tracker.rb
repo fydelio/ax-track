26bc827a5042969769e954bf78210180a78bf256
Philipp Baumann <philipp@timly.ch>
2021-10-06 14:09:49 +0200

    updated sensor_data

--- lib/ax_track/objects/tracker.rb
+++ lib/ax_track/objects/tracker.rb
@@ -2,30 +2,32 @@ module AxTrack
   class Tracker < Object
 
     def initialize(json_response)
-      @tracker_id =       json_response['id']
-      @asset_id =         json_response['asset']
-      @url =              json_response['url']
-      @active =           json_response['active']
-      @model =            json_response['model']
-      @asset_details      = Asset.new json_response['asset_details'] if json_response['asset_details']
-      @name =             json_response.dig('asset_details', 'name')
+      @tracker_id        = json_response['id']
+      @asset_id          = json_response['asset']
+      @identifier        = json_response['identifier']
+      @url               = json_response['url']
+      @active            = json_response['active']
+      @model             = json_response['model']
+      @asset_details     = Asset.new json_response['asset_details'] if json_response['asset_details']
+      @name              =  json_response.dig('asset_details', 'name')
       @last_message_timestamp = DateTime.parse(json_response['last_message_timestamp'], false) if json_response['last_message_timestamp']
-      @url =              json_response['url']
-      @user_url =         website_url
+      @url               = json_response['url']
+      @user_url          = website_url
       @last_gps_position = GPSPosition.new(json_response['last_gps_measurement'] || json_response['asset_details'])
 
-      @battery =          json_response.dig('asset_details', 'sensor_data', 'battery', 'value')
+      @battery           = json_response.dig('asset_details', 'sensor_data', 'battery', 'value')
+      sensor_data        = json_response.dig('asset_details', 'sensor_data')
 
-      sensor_data =       json_response.dig('asset_details', 'sensor_data')
-      sensor_data.delete('battery')
-      @sensor_data =      sensor_data
+      @sensor_data       = sensor_data
 
       create_getters
     end
 
     def available_sensor_data
       # returns a hash with available senson data
-      self.sensor_data.keys
+      sensor_data_temp = self.sensor_data.keys
+      sensor_data_temp = sensor_data_temp.unshift('gps') if self.respond_to? :last_gps_position
+      sensor_data_temp
     end
 
     def website_url
